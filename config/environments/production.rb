18a2e25c4cbc9fd68331bd7cc20713338b719611
Olly Headey <olly@headey.org>
2024-03-21 22:30:29 +0000

    Use env var for Postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "pagecord.com" }
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = {
-    api_token: Rails.application.credentials.postmark_api_token
+    api_token: ENV["POSTMARK_API_TOKEN"]
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
