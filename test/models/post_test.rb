79bd67aefd2fb1b49b34f7176480083093926337
Olly Headey <olly@headey.org>
2024-03-22 09:50:19 +0000

    Store a published_at with Post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "published at should be set on create if not provided" do
+    post = users(:joel).posts.create! title: "my new post", content: "this is my new post", html: false
+
+    assert_equal post.created_at, post.published_at
+  end
+
+  test "published at should be set" do
+    published_at = 1.day.ago
+    post = users(:joel).posts.create! title: "my new post", content: "this is my new post", html: false, published_at: published_at
+
+    assert_equal published_at, post.published_at
+  end
 end
