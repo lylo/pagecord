46f80ec7c1d61de814823913e073595baebbab76
Olly Headey <olly@headey.org>
2024-03-22 11:41:23 +0000

    Tidy up friendly id

---
+++ app/models/concerns/opaque_id.rb
@@ -0,0 +1,21 @@
+module OpaqueId
+  extend ActiveSupport::Concern
+
+  OFFSET = 1_000_000.freeze
+
+  included do
+    def url_title
+      title.parameterize.truncate(100)
+    end
+
+    def url_id
+      (id + OFFSET).to_s(36)
+    end
+  end
+
+  class_methods do
+    def from_url_id(url_id)
+      url_id.to_i(36) - OFFSET
+    end
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,9 @@
 class Post < ApplicationRecord
+  include OpaqueId
+
   belongs_to :user
 
   before_create :set_published_at
-
-  def url_title
-    title.parameterize.truncate(100)
-  end
-
-  def url_id
-    id.to_s(36)
-  end
 
   private
 
