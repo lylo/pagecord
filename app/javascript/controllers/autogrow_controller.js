4df0874122eb128ae249646d1c690fce028fc05e
Olly Headey <olly@headey.org>
2024-03-22 14:48:15 +0000

    Autogrow the text area

--- app/javascript/controllers/autogrow_controller.js
+++ app/javascript/controllers/autogrow_controller.js
@@ -1,31 +1,33 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   initialize() {
-    this.autogrow = this.autogrow.bind(this);
-    this.debounceTimeout = null;
+    this.autogrow = this.autogrow.bind(this)
+    this.debounceTimeout = null
   }
 
   connect() {
-    this.element.style.overflow = "hidden";
+    this.element.style.overflow = "hidden"
 
-    this.autogrow();
+    this.autogrow()
 
-    this.element.addEventListener("input", this.queueAutogrow.bind(this));
-    window.addEventListener("resize", this.queueAutogrow.bind(this));
+    this.element.addEventListener("input", this.queueAutogrow.bind(this))
+    window.addEventListener("resize", this.queueAutogrow.bind(this))
   }
 
   disconnect() {
-    window.removeEventListener("resize", this.queueAutogrow.bind(this));
+    window.removeEventListener("resize", this.queueAutogrow.bind(this))
   }
 
   queueAutogrow() {
-    clearTimeout(this.debounceTimeout);
-    this.debounceTimeout = setTimeout(this.autogrow.bind(this), 100);
+    clearTimeout(this.debounceTimeout)
+    this.debounceTimeout = setTimeout(this.autogrow.bind(this), 100)
   }
 
   autogrow() {
-    this.element.style.height = "auto";
-    this.element.style.height = `${this.element.scrollHeight}px`;
+    console.log("autogrow")
+    console.log(this.element)
+    this.element.style.height = "auto"
+    this.element.style.height = `${this.element.scrollHeight}px`
   }
 }
