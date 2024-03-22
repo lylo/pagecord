6ca46ec8af3e1f2298bd7f4d165f6ef6a5dfd4f3
Olly Headey <olly@headey.org>
2024-03-22 08:44:00 +0000

    Force mail content to be UTF-8

--- app/models/mail_parser.rb
+++ app/models/mail_parser.rb
@@ -6,7 +6,7 @@ class MailParser
   def body
     if @mail.multipart? && @mail.html_part
       document = Nokogiri::HTML(@mail.html_part.body.decoded)
-      body = document.at_css("body").inner_html.encode('utf-8')
+      body = document.at_css("body").inner_html.force_encoding('UTF-8').encode('utf-8')
 
       sanitizer = Rails::HTML5::SafeListSanitizer.new
       sanitizer.sanitize(body, tags: %w(a abbr b blockquote br cite code div em h1 h2 h3 h4 h5 h6 hr i li mark ol p pre strong u ul), attributes: %w(href))
