4300af24fa4c774e5d242466c266dfc3967256ee
Olly Headey <olly@headey.org>
2024-03-22 09:08:58 +0000

    Minimum 4 letter username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :access_requests, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   private
