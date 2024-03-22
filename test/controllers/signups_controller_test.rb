c0fe883dadfdf1e4a08f2be8c056a6321ffc4a10
Olly Headey <olly@headey.org>
2024-03-22 16:32:29 +0000

    Formatting and tidy up

--- test/controllers/signups_controller_test.rb
+++ test/controllers/signups_controller_test.rb
@@ -1,14 +1,14 @@
 require "test_helper"
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class SignupsControllerTest < ActionDispatch::IntegrationTest
   test "should create user and redirect to posts path" do
     assert_difference("User.count") do
       assert_emails 1 do
-        post users_url, params: { user: { username: "testuser", email: "test@example.com" } }
+        post signups_url, params: { user: { username: "testuser", email: "test@example.com" } }
       end
     end
 
-    assert_redirected_to user_posts_path(User.last.username)
+    assert_redirected_to thanks_signups_path
 
     assert_equal "testuser", User.last.username
     assert_equal "test@example.com", User.last.email
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should not create user with invalid params" do
     assert_no_difference("User.count") do
       assert_emails 0 do
-        post users_url, params: { user: { username: "", email: "" } }
+        post signups_url, params: { user: { username: "", email: "" } }
       end
     end
 
