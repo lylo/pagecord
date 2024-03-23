2220236038261bcd168fb198004dddf178b50807
Olly Headey <olly@headey.org>
2024-03-23 21:38:03 +0000

    Better login and verification emails

--- app/mailers/account_verification_mailer.rb
+++ app/mailers/account_verification_mailer.rb
@@ -4,13 +4,13 @@ class AccountVerificationMailer < ApplicationMailer
     @user = params[:user]
     @access_request = @user.access_requests.create!
 
-    mail(to: @user.email, subject: "📃 Verify your Pagecord email address")
+    mail(to: @user.email, subject: "Verify your Pagecord email address")
   end
 
   def login
     @user = params[:user]
     @access_request = @user.access_requests.create!
 
-    mail(to: @user.email, subject: "📃 Log into your Pagecord account")
+    mail(to: @user.email, subject: "Log into your Pagecord account")
   end
 end
