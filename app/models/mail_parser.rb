4ca92640e26c02225b9940be2f039454a2e83213
Olly Headey <olly@headey.org>
2024-03-23 17:49:16 +0000

    Support strikethrough

--- app/models/mail_parser.rb
+++ app/models/mail_parser.rb
@@ -20,7 +20,7 @@ class MailParser
       body = document.at_css("body").inner_html
 
       sanitizer = Rails::HTML5::SafeListSanitizer.new
-      sanitizer.sanitize(body, tags: %w(a abbr b blockquote br cite code div em h1 h2 h3 h4 h5 h6 hr i li mark ol p pre strong u ul), attributes: %w(href))
+      sanitizer.sanitize(body, tags: %w(a abbr b blockquote br cite code del div em h1 h2 h3 h4 h5 h6 hr i li mark ol p pre s strike strong u ul), attributes: %w(href))
     elsif @mail.multipart? && @mail.text_part
       @mail.text_part.body.decoded
     else
