c2f2d6f41167692e798992023fb5a779c93852fe
Olly Headey <olly@headey.org>
2024-03-25 10:00:38 +0000

    Username must be alphanumeric and underscores

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :access_requests, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }, format: { with: /\A[a-zA-Z0-9_]+\z/ }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   private
