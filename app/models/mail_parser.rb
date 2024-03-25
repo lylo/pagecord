0f7e6deadb986b234a7d004d621570f7bf899b19
Olly Headey <olly@headey.org>
2024-03-25 13:25:38 +0000

    Allow span

--- app/models/mail_parser.rb
+++ app/models/mail_parser.rb
@@ -36,7 +36,7 @@ class MailParser
       body = document.at_css("body").inner_html
     end
 
-    ALLOWED_TAGS =  %w(a abbr b blockquote br cite code del div em h1 h2 h3 h4 h5 h6 hr i li mark ol p pre s strike strong u ul)
+    ALLOWED_TAGS =  %w(a abbr b blockquote br cite code del div em h1 h2 h3 h4 h5 h6 hr i li mark ol p pre s span strike strong u ul)
     ALLOWED_ATTRIBUTES = %w(href class)
 
     def sanitize(html)
