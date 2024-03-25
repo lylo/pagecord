d2484147e5ba21284084086727b7b6dc584392ad
Olly Headey <olly@headey.org>
2024-03-25 10:09:06 +0000

    Additional reserved usernames

--- app/models/username.rb
+++ app/models/username.rb
@@ -1,6 +1,6 @@
 class Username
   RESERVED = %w{ pricing about contact faq terms privacy careers blog
-                 login signup feed rss pagecord }
+                 login signup feed rss pagecord admin support help}
 
   def self.reserved?(username)
     RESERVED.include?(username)
