c78cf82e65146c27ea0de173f9b9327c44e93f4f
Olly Headey <olly@headey.org>
2024-03-25 15:57:39 +0000

    Attempt to be clever and detect monospace so we can render code blocks. What could possibly go wrong?

--- app/models/mail_parser.rb
+++ app/models/mail_parser.rb
@@ -15,8 +15,8 @@ class MailParser
   def body
     if html?
       html = decode(@mail.html_part.decoded)
-      body = extract_body_tag(html)
-      sanitize(body)
+      body = extract_body_tag(process_html(html))
+      sanitize(body).strip
     elsif @mail.multipart? && @mail.text_part
       @mail.text_part.body.decoded
     else
@@ -33,7 +33,42 @@ class MailParser
 
     def extract_body_tag(html)
       document = Nokogiri::HTML(html)
-      body = document.at_css("body").inner_html
+      document.at_css("body").inner_html
+    end
+
+    def process_html(html)
+      process_monospace(html)
+    end
+
+    def process_monospace(html)
+      document = Nokogiri::HTML(html)
+
+      replace_style_tags_with_code_tags(document)
+      replace_font_tags_with_code_tags(document)
+
+      document.to_html
+    end
+
+    # Fastmail wraps monospace lines in <span style="font-family: monospace"> tags
+    def replace_style_tags_with_code_tags(document)
+      document.css("div[style], span[style]").each do |element|
+        if element['style'].downcase.include?('font-family: monospace')
+          code = Nokogiri::XML::Node.new "code", document
+          code.children = element.children
+          element.replace code
+        end
+      end
+    end
+
+    # Gmail wraps monospace in <font face="monospace"> tags
+    def replace_font_tags_with_code_tags(document)
+      document.css("font[face]").each do |element|
+        if element["face"].downcase == "monospace"
+          tag = Nokogiri::XML::Node.new "code", document
+          tag.inner_html = element.inner_html
+          element.replace tag    # replace <font> tag with <code>
+        end
+      end
     end
 
     ALLOWED_TAGS =  %w(a abbr b blockquote br cite code del div em h1 h2 h3 h4 h5 h6 hr i li mark ol p pre s span strike strong u ul)
