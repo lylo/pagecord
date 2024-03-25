4739c34fc1276d6d20bd6c85f218b28b57c7a9f2
Olly Headey <olly@headey.org>
2024-03-25 10:20:30 +0000

    Tidy up parsing code

--- app/models/mail_parser.rb
+++ app/models/mail_parser.rb
@@ -14,17 +14,33 @@ class MailParser
 
   def body
     if html?
-      html_decoder = HTMLEntities.new
-      decoded_body = html_decoder.decode(@mail.html_part.decoded).force_encoding('UTF-8')
-      document = Nokogiri::HTML(decoded_body)
-      body = document.at_css("body").inner_html
-
-      sanitizer = Rails::HTML5::SafeListSanitizer.new
-      sanitizer.sanitize(body, tags: %w(a abbr b blockquote br cite code del div em h1 h2 h3 h4 h5 h6 hr i li mark ol p pre s strike strong u ul), attributes: %w(href))
+      html = decode(@mail.html_part.decoded)
+      body = extract_body_tag(html)
+      sanitize(body)
     elsif @mail.multipart? && @mail.text_part
       @mail.text_part.body.decoded
     else
       @mail.decoded
     end
   end
+
+  private
+
+    def decode(html)
+      html_decoder = HTMLEntities.new
+      decoded_body = html_decoder.decode(html).force_encoding('UTF-8')
+    end
+
+    def extract_body_tag(html)
+      document = Nokogiri::HTML(html)
+      body = document.at_css("body").inner_html
+    end
+
+    ALLOWED_TAGS =  %w(a abbr b blockquote br cite code del div em h1 h2 h3 h4 h5 h6 hr i li mark ol p pre s strike strong u ul)
+    ALLOWED_ATTRIBUTES = %w(href)
+
+    def sanitize(html)
+      sanitizer = Rails::HTML5::SafeListSanitizer.new
+      sanitizer.sanitize html, tags: ALLOWED_TAGS, attributes: ALLOWED_ATTRIBUTES
+    end
 end
