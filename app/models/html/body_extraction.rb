4e144237fe6c25ecb1f9154d0e7c27c4d8ad3e88
Olly Headey <olly@headey.org>
2024-03-26 15:13:12 +0000

    Don't bail if there's no body

--- app/models/html/body_extraction.rb
+++ app/models/html/body_extraction.rb
@@ -2,7 +2,12 @@ module Html
   class BodyExtraction < Transformation
     def transform(html)
       document = Nokogiri::HTML(html)
-      document.at_css("body").inner_html
+      body_tag = document.at_css("body")
+      if body_tag
+        body_tag.inner_html
+      else
+        ""
+      end
     end
   end
 end
