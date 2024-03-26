d05882c6700c77970b2988f38d00036f5bff120d
Olly Headey <olly@headey.org>
2024-03-26 16:42:41 +0000

    Move logic into MailParser

--- app/mailboxes/posts_mailbox.rb
+++ app/mailboxes/posts_mailbox.rb
@@ -1,6 +1,4 @@
 class PostsMailbox < ApplicationMailbox
-  include ActionView::Helpers::SanitizeHelper
-
   def process
     return unless mail.to.present? && mail.from.present?
 
@@ -8,6 +6,7 @@ class PostsMailbox < ApplicationMailbox
     from = mail.from.first.downcase
     reply_to = mail.reply_to.first.downcase if mail.reply_to.present?
     Rails.logger.info "Received mail from: #{from} to: #{recipient} (reply-to: #{reply_to})"
+    Rails.logger.info "Message ID: #{mail.message_id}"
 
     if reply_to.present? && reply_to != from
       Rails.logger.warn "Reply-To and From are inconsistent" and return
@@ -18,21 +17,18 @@ class PostsMailbox < ApplicationMailbox
     end
 
     if user = User.kept.find_by(email: from, delivery_email: recipient)
-      Rails.logger.info "Creating post from user: #{user.id}"
+      begin
+        parser = MailParser.new(mail)
 
-      parser = MailParser.new(mail)
-      title = mail.subject&.strip
-      content = parser.body
-
-      content_blank = if parser.html?
-        strip_tags(content)&.strip.blank?
-      else
-        content&.strip.blank?
+        unless parser.is_blank?
+          Rails.logger.info "Creating post from user: #{user.id}"
+          user.posts.create!(title: parser.subject, content: parser.body, html: parser.html?, published_at: mail.date)
+        end
+      rescue => e
+        Rails.logger.warn "Unable to parse email: #{e.message}"
       end
-
-      return if content_blank && title.blank?
-
-      user.posts.create!(title: title, content: content, html: parser.html?, published_at: mail.date)
+    else
+      Rails.logger.warn "User not found. From: #{from}, To: #{recipient}"
     end
   end
 
--- app/models/mail_parser.rb
+++ app/models/mail_parser.rb
@@ -1,4 +1,5 @@
 class MailParser
+  include ActionView::Helpers::SanitizeHelper
 
   def initialize(mail)
     @mail = mail
@@ -11,28 +12,39 @@ class MailParser
     ]
   end
 
+  def subject
+    @title ||= @mail.subject&.strip
+  end
+
+  def body
+    @body ||= parse_body
+  end
+
   def html?
     @mail.multipart? && @mail.html_part
   end
 
-  def plain_text?
-    !html?
+  def is_blank?
+    subject.blank? && strip_tags(body)&.strip.blank?
   end
 
-  def body
-    if html?
-      html = @mail.html_part.decoded
-      @pipeline.each do |transformation|
-        html = transformation.transform(html)
+  private
+
+    def parse_body
+      if @mail.multipart?
+        if @mail.html_part
+          html = @mail.html_part.decoded
+          @pipeline.each do |transformation|
+            html = transformation.transform(html)
+          end
+          html
+        elsif @mail.text_part
+          @mail.text_part.body.decoded
+        end
+      elsif @mail.content_type =~ /text\/plain/
+        @mail.decoded
+      else
+        raise "Unknown content type #{mail.content_type}"
       end
-      html
-    elsif @mail.multipart? && @mail.text_part
-      @mail.text_part.body.decoded
-    elsif @mail.content_type =~ /text\/plain/
-      @mail.decoded
-    else
-      Rails.logger.warn "Unable to parse email content type: #{@mail.content_type}"
-      nil
     end
-  end
 end
