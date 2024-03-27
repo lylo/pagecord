35b7d1c7dc16f5e00f119e1fb4b01218935254bf
Olly Headey <olly@headey.org>
2024-03-27 18:08:53 +0000

    Slightly improved app index page

--- app/controllers/app/posts_controller.rb
+++ app/controllers/app/posts_controller.rb
@@ -4,7 +4,7 @@ class App::PostsController < AppController
   before_action :load_user
 
   def index
-    @pagy, @posts =  pagy(Current.user.posts.order(created_at: :desc))
+    @pagy, @posts =  pagy(Current.user.posts.order(created_at: :desc), items: 15)
   end
 
   def destroy
