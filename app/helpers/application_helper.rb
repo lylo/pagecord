f03cb48f5229e42095a407bd5512635ccc867a5a
Olly Headey <olly@headey.org>
2024-03-27 18:36:33 +0000

    Tidy up logic in head partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  def page_title
+    base_title = "Pagecord"
+    if @user&.username
+      "#{base_title} | @#{@user.username}"
+    elsif content_for?(:title)
+      "#{base_title} | #{content_for(:title)}"
+    else
+      base_title
+    end
+  end
+
+  def post_title(post)
+    @post_title ||= begin
+      if post.title.present?
+        post.title.truncate(100)
+      else
+        sanitized_content = strip_tags(post.content.truncate(140))
+        if sanitized_content.blank?
+          "Untitled"
+        else
+          sanitized_content
+        end
+      end
+    end
+  end
+
+  def user_bio(user)
+    @user_bio ||= begin
+      bio = user.bio.present? ? strip_tags(user.bio.truncate(140)) : ""
+      "Posts from @#{user.username}. #{bio}"
+    end
+  end
 end
