bde904797a9374bab67b6694e26c306ee783cb4f
Olly Headey <olly@headey.org>
2024-03-27 10:40:05 +0000

    Show sensible title in admin area post list

--- app/helpers/app/posts_helper.rb
+++ app/helpers/app/posts_helper.rb
@@ -1,2 +1,14 @@
 module App::PostsHelper
+  def post_title(post)
+    if post.title.present?
+      post.title.truncate(100)
+    else
+      sanitized_content = strip_tags(post.content.truncate(140))
+      if sanitized_content.blank?
+        "Untitled"
+      else
+        sanitized_content
+      end
+    end
+  end
 end
