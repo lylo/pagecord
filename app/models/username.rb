fa54494f7d97fca7b9a6dff621bf53a2ba3a7fb9
Olly Headey <olly@headey.org>
2024-03-28 16:08:07 +0000

    Allow 3-letter usernames

--- app/models/username.rb
+++ app/models/username.rb
@@ -1,10 +1,10 @@
 class Username
-  RESERVED = %w{ about admin blog careers contact faq feed
+  RESERVED = %w{ about admin app blog careers contact faq feed
                  help login pagecord privacy pricing rss
                  signup support terms }
 
   FORMAT = /\A[a-zA-Z0-9_]+\z/
-  MIN_LENGTH = 4
+  MIN_LENGTH = 3
   MAX_LENGTH = 20
 
   def self.reserved?(username)
