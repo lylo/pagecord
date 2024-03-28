59fc9c4d74122566c7d31074ef5c014da0c51d61
Olly Headey <olly@headey.org>
2024-03-28 16:31:24 +0000

    Hook up Sidekiq dashboard

--- app/models/username.rb
+++ app/models/username.rb
@@ -1,7 +1,7 @@
 class Username
   RESERVED = %w{ about admin app blog careers contact faq feed
-                 help login pagecord privacy pricing rss
-                 signup support terms }
+                 help jobs login pagecord privacy pricing rss
+                 sidekiq signup support terms  }
 
   FORMAT = /\A[a-zA-Z0-9_]+\z/
   MIN_LENGTH = 3
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,27 @@
+require "sidekiq/web"
+
+class SidekiqAdminConstraint
+  def matches?(request)
+    if current_user = User.kept.find(request.session[:user_id])
+      ENV["ADMIN_USERNAME"] == current_user.username &&
+      ENV["ADMIN_DELIVERY_EMAIL"] == current_user.delivery_email
+    else
+      false
+    end
+  rescue
+    false
+  end
+end
+
 Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  constraints SidekiqAdminConstraint.new do
+    mount Sidekiq::Web => '/admin/sidekiq'
+  end
 
   get "/404", to: "errors#not_found"
   get "/422", to: "errors#unacceptable"
