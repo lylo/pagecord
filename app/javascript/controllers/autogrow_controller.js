4127f0da7682270ed9f7ffb42e4906ede15886ab
Olly Headey <olly@headey.org>
2024-04-03 09:36:59 +0100

    Remove console log

--- app/javascript/controllers/autogrow_controller.js
+++ app/javascript/controllers/autogrow_controller.js
@@ -25,8 +25,6 @@ export default class extends Controller {
   }
 
   autogrow() {
-    console.log("autogrow")
-    console.log(this.element)
     this.element.style.height = "auto"
     this.element.style.height = `${this.element.scrollHeight}px`
   }
