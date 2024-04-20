62646a9434227d15e826617ed9904b97f6dc81d3
Olly Headey <olly@headey.org>
2024-04-20 21:31:42 +0100

    Ignore discarded at user's posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,6 +2,6 @@ class Admin::PostsController < AdminController
   include Pagy::Backend
 
   def index
-    @pagy, @posts = pagy(Post.all.includes(:user).order(created_at: :desc), items: 15)
+    @pagy, @posts = pagy(Post.joins(:user).where(users: { discarded_at: nil }).includes(:user).order(created_at: :desc), items: 15)
   end
 end
