f70f7cc1c452499cc1f4c7b7e4233151b3fa8c34
Olly Headey <olly@headey.org>
2024-05-03 15:57:23 +0100

    Initial implementation of custom domains

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,6 +1,8 @@
 class Users::PostsController < ApplicationController
   include Pagy::Backend
 
+  skip_before_action :domain_check      # PostsController can operate with a custom domain
+
   before_action :load_user, :verification
 
   def index
@@ -22,9 +24,13 @@ class Users::PostsController < ApplicationController
   private
 
     def load_user
-      @user ||= User.kept.find_by(username: user_params[:username])
+      @user ||= if user_params[:username]
+        User.kept.find_by(username: user_params[:username])
+      else
+        user_from_custom_domain
+      end
 
-      redirect_to root_path, alert: "User not found" if @user.nil?
+      redirect_home_with_forbidden if @user.nil?
     end
 
     def user_params
@@ -33,5 +39,11 @@ class Users::PostsController < ApplicationController
 
     def verification
       redirect_to root_path if !@user&.verified?
+    end
+
+    def user_from_custom_domain
+      if custom_domain_request?
+        User.kept.find_by(custom_domain: request.host)
+      end
     end
 end
---
+++ app/helpers/routing_helper.rb
@@ -0,0 +1,46 @@
+module RoutingHelper
+
+  def post_link(post, type)
+    if custom_domain_request?
+      if post.url_title.present?
+        send("custom_post_with_title_#{type}", post.url_title, post.url_id)
+      else
+        send("custom_post_without_title_#{type}", post.url_id)
+      end
+    else
+      if post.url_title.present?
+        send("post_with_title_#{type}", post.user.username, post.url_title, post.url_id)
+      else
+        send("post_without_title_#{type}", post.user.username, post.url_id)
+      end
+    end
+  end
+
+  def post_path(post)
+    post_link(post, 'path')
+  end
+
+  def post_url(post)
+    post_link(post, 'url')
+  end
+
+  def user_home(user, type)
+    if custom_domain_request?
+      send("custom_user_posts_#{type}")
+    else
+      send("user_posts_#{type}", username: user.username)
+    end
+  end
+
+  def user_home_path(user)
+    user_home(user, 'path')
+  end
+
+  def user_home_url(user)
+    user_home(user, 'url')
+  end
+
+  def custom_domain_request?
+    !%w[pagecord.com localhost].include?(request.host)
+  end
+end
