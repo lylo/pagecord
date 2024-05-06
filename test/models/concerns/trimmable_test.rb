415112dbd4065f1131557729e3fb48069d27c99e
Olly Headey <olly@headey.org>
2024-05-06 20:26:52 +0100

    Use published_at not updated_at!

--- test/models/concerns/trimmable_test.rb
+++ test/models/concerns/trimmable_test.rb
@@ -4,9 +4,8 @@ class TrimmableTest < ActiveSupport::TestCase
 
   test "should strip empty tags before save" do
     post = posts(:two)
-    post.body = "<div><br><p>Test</p>\n<br><p><br></p><br><div></div></div>  "
+    post.body = "<div><br><p>Test</p><br><br>\n<p><br></p><br><div></div>\n\n\n</div>"
     post.save!
-
     assert_equal "<div><br><p>Test</p></div>", post.body.body.to_s.gsub("\n", "")
   end
 end
