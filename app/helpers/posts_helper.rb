a84aca1cdf482a41450ac833eefa7b17c5f567c5
Olly Headey <olly@headey.org>
2024-05-06 11:32:26 +0100

    Try and improve RSS feed

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,11 @@
 module PostsHelper
   include Pagy::Frontend
+
+  def without_action_text_image_wrapper(html)
+    # Regular expression to match the action-text-attachment wrapper
+    attachment_regex = /<action-text-attachment[^>]*>(.*?)<\/action-text-attachment>/m
+
+    # Replace the ActionText attachment wrapper with just the image tag
+    html.gsub(attachment_regex) { |match| $1.gsub(/<figure[^>]*>/, '').gsub(/<\/figure>/, '') }
+  end
 end
