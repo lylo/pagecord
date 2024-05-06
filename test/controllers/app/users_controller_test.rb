6251196de0d9e4e3d970aa106a93a57d9f5a2678
Olly Headey <olly@headey.org>
2024-05-06 14:13:57 +0100

    Extract Hatchbox domain logic to it's own class and job

--- test/controllers/app/users_controller_test.rb
+++ test/controllers/app/users_controller_test.rb
@@ -3,18 +3,61 @@ require "test_helper"
 class App::UsersControllerTest < ActionDispatch::IntegrationTest
   include AuthenticatedTest
 
-  test "delete account" do
-    user = users(:joel)
-    login_as user
+  setup do
+    @user = users(:joel)
+    login_as @user
+  end
 
+  test "delete account" do
     assert_performed_jobs 1 do
       assert_difference("User.kept.count", -1) do
-        delete app_user_url(user)
+        delete app_user_url(@user)
       end
     end
 
     assert_redirected_to root_url
-    refute User.kept.exists?(user.id)
-    assert User.exists?(user.id) # soft delete
+    refute User.kept.exists?(@user.id)
+    assert User.exists?(@user.id) # soft delete
+  end
+
+  test "should update user bio" do
+    patch app_user_url(@user), params: { user: { bio: "New bio" } }, as: :turbo_stream
+
+    assert_response :success
+    assert_equal "New bio", @user.reload.bio
+  end
+
+  test "should update user custom domain" do
+    patch app_user_url(@user), params: { user: { custom_domain: "newdomain.com" } }, as: :turbo_stream
+
+    assert_response :success
+    assert_equal "newdomain.com", @user.reload.custom_domain
+  end
+
+  test "should call hatchbox when adding custom domain" do
+    assert_performed_jobs 1 do
+      patch app_user_url(@user), params: { user: { custom_domain: "newdomain.com" } }, as: :turbo_stream
+    end
+  end
+
+  test "should call hatchbox when removing custom domain" do
+    user = users(:annie)
+    login_as user
+
+    assert_performed_jobs 1 do
+      patch app_user_url(user), params: { user: { custom_domain: "" } }, as: :turbo_stream
+    end
+  end
+
+  test "should not call hatchbox if nil custom domain is changed to empty string" do
+    assert_performed_jobs 0 do
+      patch app_user_url(@user), params: { user: { custom_domain: "" } }, as: :turbo_stream
+    end
+  end
+
+  test "should not call hatchbox if custom domain is changed to same value" do
+    assert_performed_jobs 0 do
+      patch app_user_url(users(:annie)), params: { user: { custom_domain: users(:annie).custom_domain } }, as: :turbo_stream
+    end
   end
 end
