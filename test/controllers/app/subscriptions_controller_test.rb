9a6ab2c11a515068a5f860c78575b2f7faa73aba
Olly Headey <olly@headey.org>
2024-05-14 11:51:00 +0100

    Billing test

---
+++ test/controllers/app/subscriptions_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+require "minitest/autorun"
+
+class SubscriptionsControllerTest < ActionDispatch::IntegrationTest
+  include AuthenticatedTest
+
+  setup do
+    @user = users(:joel)
+    login_as @user
+  end
+
+  test "should get index" do
+    get app_subscriptions_path
+    assert_response :success
+  end
+
+  test "should get thanks" do
+    get thanks_app_subscriptions_path
+    assert_response :success
+  end
+
+  test "should cancel subscription" do
+    paddle_api_mock = Minitest::Mock.new
+    paddle_api_mock.expect :cancel_subscription, true, [@user.subscription.paddle_subscription_id]
+
+    PaddleApi.stub :new, paddle_api_mock do
+      delete app_subscription_url(@user.subscription)
+    end
+
+    assert_redirected_to app_account_path
+    assert @user.subscription.reload.cancelled?
+  end
+
+  test "should get cancel_confirm" do
+    get cancel_confirm_app_subscriptions_path
+    assert_response :success
+  end
+end
