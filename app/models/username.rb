3633cb6d65784d2a93f7f7ed3587653f9426c18b
Olly Headey <olly@headey.org>
2024-06-27 18:04:28 +0100

    Stop allowing full stops in usernames

--- app/models/username.rb
+++ app/models/username.rb
@@ -3,7 +3,7 @@ class Username
                  help jobs login pagecord privacy pricing rss
                  sidekiq signup support terms  }
 
-  FORMAT = /\A[a-zA-Z0-9_\.]+\z/
+  FORMAT_REGEX = /\A[a-zA-Z0-9_]+\z/
   MIN_LENGTH = 3
   MAX_LENGTH = 20
 
@@ -13,21 +13,10 @@ class Username
 
   def self.valid_format?(username)
     # specifically allowed characters
-    return false unless username =~ /\A[a-zA-Z0-9_.]+\z/
-
-    # cannot start or end with a period
-    return false if username.start_with?('.') || username.end_with?('.')
-
-    # Only one period allowed
-    return false if username.count('.') > 1
+    return false unless username =~ FORMAT_REGEX
 
     # Only one underscore allowed
     return false if username.count('_') > 1
-
-    # Period must be between alphanumeric characters if present
-    if username.include?('.')
-      return false unless username =~ /[a-zA-Z0-9]\.[a-zA-Z0-9]/
-    end
 
     true
   end
