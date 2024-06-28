fec26ef10f1770ecc93408e0617d1707dcd377d0
Olly Headey <olly@headey.org>
2024-06-28 12:01:08 +0100

    Fix issue with remove_trailing_empty_nodes removing legit <br> tags which are surrounded by text

--- test/models/concerns/trimmable_test.rb
+++ test/models/concerns/trimmable_test.rb
@@ -4,8 +4,8 @@ class TrimmableTest < ActiveSupport::TestCase
 
   test "should strip empty tags before save" do
     post = posts(:two)
-    post.content = "<div><br><p>Test</p><br><br>\n<p><br></p><br><div></div>\n\n\n</div>"
+    post.content = "<div><br><p>Test</p><img src='test.jpg'><br><br>hello\n<br><div></div>\n\n\n</div><p></p><div></div>"
     post.save!
-    assert_equal "<div><br><p>Test</p></div>", post.content.to_s.gsub("\n", "")
+    assert_equal "<div><br><p>Test</p><img src=\"test.jpg\"><br><br>hello<br><div></div></div>", post.content.to_s.gsub("\n", "")
   end
 end
---
+++ test/models/html/sanitize_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class Html::SanitizeTest < ActiveSupport::TestCase
+
+  test "should convert monospace style tag to code block" do
+    html = <<~HTML
+      <div><p>hello, world!</p><br><action-text-attachment content-type="image/jpeg" filename="IMG_4475.JPG" filesize="158121" width="652" height="1000" previewable="true"><figure class="attachment attachment--preview attachment--JPG"><img src="https://example.com/image.jpeg"></figure></action-text-attachment><br></div>
+    HTML
+
+    transformed_html = Html::Sanitize.new.transform(html)
+
+    expected_html = <<~HTML
+      <div>
+      <p>hello, world!</p>
+      <br><action-text-attachment content-type="image/jpeg" filename="IMG_4475.JPG"></action-text-attachment><br>
+      </div>
+    HTML
+    assert_equal expected_html.strip, transformed_html
+  end
+end
