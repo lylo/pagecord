c6bad9e17f1b21e1f5177bc3c01311b2bbafcb37
Olly Headey <olly@headey.org>
2024-08-26 23:23:11 +0100

    Use importmaps for media embeds

--- app/javascript/embeds/youtube.js
+++ app/javascript/embeds/youtube.js
@@ -1,33 +1,33 @@
-import MediaSite from './media_site'
+import MediaSite from "media_site"
 
 class YouTube extends MediaSite {
   constructor() {
     super(
       /(?:https:\/\/www\.youtube\.com\/watch\?v=|https:\/\/youtu\.be\/)([a-zA-Z0-9_-]+)/,
       (url) => {
-        const match = url.match(this.regex);
+        const match = url.match(this.regex)
         if (match) {
-          const videoId = match[1];
-          return `https://www.youtube.com/embed/${videoId}`;
+          const videoId = match[1]
+          return `https://www.youtube.com/embed/${videoId}`
         }
-        return null;
+        return null
       },
       (embedUrl) => {
-        const div = document.createElement('div');
-        div.className = "aspect-w-16 aspect-h-9";
+        const div = document.createElement('div')
+        div.className = "aspect-w-16 aspect-h-9"
 
-        const iframe = document.createElement('iframe');
-        iframe.className = "mx-auto w-full h-full";
-        iframe.src = embedUrl;
-        iframe.allow = "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture";
-        iframe.loading = "lazy";
+        const iframe = document.createElement('iframe')
+        iframe.className = "mx-auto w-full h-full"
+        iframe.src = embedUrl
+        iframe.allow = "autoplay clipboard-write encrypted-media fullscreen picture-in-picture"
+        iframe.loading = "lazy"
 
-        div.appendChild(iframe);
+        div.appendChild(iframe)
 
-        return div;
+        return div
       }
-    );
+    )
   }
 }
 
-export default YouTube;
+export default YouTube
