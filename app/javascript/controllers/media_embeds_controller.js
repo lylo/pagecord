00f8183f72da33f6c2f2e695828dc9b6f464b9cf
Olly Headey <olly@headey.org>
2024-08-27 00:05:16 +0100

    No need to embed plain text links since they're autolinked!

--- app/javascript/controllers/media_embeds_controller.js
+++ app/javascript/controllers/media_embeds_controller.js
@@ -15,7 +15,6 @@ export default class extends Controller {
     articles.forEach(article => {
       this.mediaSites.forEach(site => {
         this.embedMediaLinks(article, site)
-        this.embedPlainTextLinks(article, site)
       })
     })
   }
@@ -36,33 +35,4 @@ export default class extends Controller {
     })
   }
 
-  embedPlainTextLinks(article, site) {
-    const walker = document.createTreeWalker(article, NodeFilter.SHOW_TEXT, null, false)
-    let node
-
-    while (node = walker.nextNode()) {
-      const text = node.nodeValue
-      const match = text.match(site.regex)
-
-      if (match) {
-        const url = match[0]
-        const embedUrl = site.getEmbedUrl(url)
-
-        if (embedUrl) {
-          const iframe = site.createEmbedIframe(embedUrl)
-          const span = document.createElement('span')
-          span.appendChild(iframe)
-
-          const parts = text.split(url)
-          node.nodeValue = parts[0]
-          node.parentNode.insertBefore(span, node.nextSibling)
-
-          if (parts[1]) {
-            const remainder = document.createTextNode(parts[1])
-            node.parentNode.insertBefore(remainder, span.nextSibling)
-          }
-        }
-      }
-    }
-  }
 }
