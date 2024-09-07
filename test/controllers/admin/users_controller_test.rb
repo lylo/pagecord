e52cb00c6e4a5a2a81deaea9a4111eeb2f670545
Olly Headey <olly@headey.org>
2024-09-07 13:24:42 +0100

    Allow users to be discarded from the admin area

---
+++ test/controllers/admin/users_controller_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
+  include AuthenticatedTest
+
+  setup do
+    @user = users(:joel)
+
+    login_as @user
+  end
+
+  test "should not discard current user" do
+    delete admin_user_url(@user)
+
+    assert_redirected_to admin_stats_path
+    assert_equal "You can't discard yourself", flash[:notice]
+    assert_not @user.reload.discarded?
+  end
+
+  test "should not discard premium user" do
+    user = users(:annie)
+    assert user.is_premium?
+
+    delete  admin_user_url(user)
+
+    assert_redirected_to admin_stats_path
+    assert_equal "You can't discard a premium user", flash[:notice]
+    assert_not user.reload.discarded?
+  end
+
+  test "should discard regular user" do
+    user = users(:vivian)
+    assert_difference('User.kept.count', -1) do
+      delete admin_user_url(user)
+    end
+
+    assert_redirected_to admin_stats_path
+    assert_equal "User was successfully discarded", flash[:notice]
+    assert user.reload.discarded?
+  end
+end
