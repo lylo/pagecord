7dc5d0e64c6d8378ce2e019e5c3f99705f9a01e0
Olly Headey <olly@headey.org>
2024-11-01 14:14:04 +0000

    Edit published_at

--- test/controllers/app/posts_controller_test.rb
+++ test/controllers/app/posts_controller_test.rb
@@ -25,6 +25,22 @@ class App::PostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to app_posts_url
   end
 
+  test "should show edit post page" do
+    get edit_app_post_url(@user.posts.first)
+    assert_response :success
+  end
+
+  test "should update post" do
+    patch app_post_url(@user.posts.first), params: {
+      post: { title: "New Title", content: "New content", published_at: 1.month.ago.to_date }
+    }
+
+    assert_redirected_to app_posts_url
+    assert_equal "New Title", @user.posts.first.title
+    assert_equal "New content", @user.posts.first.content.to_s.strip
+    assert_equal 1.month.ago.to_date, @user.posts.first.published_at
+  end
+
   test "app area should be inaccessible on custom domain" do
     post = posts(:four)
     login_as post.user
