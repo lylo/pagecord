5af62a880fe4e61ab5895466b05d8e1b961c6dd4
Olly Headey <olly@headey.org>
2024-11-03 21:53:25 +0000

    OpenGraphImageJob should use ids not serialised models

--- app/jobs/generate_open_graph_image_job.rb
+++ app/jobs/generate_open_graph_image_job.rb
@@ -1,7 +1,9 @@
 class GenerateOpenGraphImageJob < ApplicationJob
   queue_as :default
 
-  def perform(post)
-    OpenGraphImage.from_post(post)&.save!
+  def perform(post_id)
+    if post = Post.find(post_id)
+      OpenGraphImage.from_post(post)&.save!
+    end
   end
 end
