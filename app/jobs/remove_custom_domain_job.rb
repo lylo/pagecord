4140be4ee9967b977eb9115a25ccaaebf6ec7f6c
Olly Headey <olly@headey.org>
2024-11-04 10:21:27 +0000

    Handing error scenario in custom domain job

--- app/jobs/remove_custom_domain_job.rb
+++ app/jobs/remove_custom_domain_job.rb
@@ -2,12 +2,12 @@ class RemoveCustomDomainJob < ApplicationJob
   queue_as :default
 
   def perform(user_id, domain)
-    user = User.find(user_id)
+    if user = User.find(user_id)
+      Rails.logger.info "Removing custom domain #{domain} for user #{user.username}"
 
-    Rails.logger.info "Removing custom domain #{domain} for user #{user.username}"
-
-    if Rails.env.production?
-      HatchboxDomainApi.new(user).remove_domain(domain)
+      if Rails.env.production?
+        HatchboxDomainApi.new(user).remove_domain(domain)
+      end
     end
   end
 end
