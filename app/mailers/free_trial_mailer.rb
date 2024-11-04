7b1e29ab15b66805ba222ffc6882af983f935387
Olly Headey <olly@headey.org>
2024-11-04 10:22:52 +0000

    Remove free trial mailer

--- app/mailers/free_trial_mailer.rb
+++
@@ -1,7 +0,0 @@
-class FreeTrialMailer < ApplicationMailer
-  def premium_only
-    @user = params[:user]
-
-    mail(to: @user.email, subject: "📣 Pagecord is now premium-only")
-  end
-end
--- test/mailers/previews/free_trial_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/account_verification_mailer
-class FreeTrialMailerPreview < ActionMailer::Preview
-  def premium_only
-    FreeTrialMailer.with(user: User.first).premium_only
-  end
-end
