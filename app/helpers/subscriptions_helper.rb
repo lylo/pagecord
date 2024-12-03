fc453bacee31340a13122ac7a00d2591c835f5ef
Olly Headey <olly@headey.org>
2024-12-03 15:30:21 +0000

    _url not _path 🤦‍♂️

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -9,7 +9,7 @@ module SubscriptionsHelper
     {
       items: paddle_data_items,
       allow_logout: false,
-      success_url: thanks_app_settings_subscriptions_path,
+      success_url: thanks_app_settings_subscriptions_url,
       custom_data: { user_id: Current.user.id }.to_json
     }
   end
