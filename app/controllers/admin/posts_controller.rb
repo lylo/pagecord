e6ebcdc75b98de3c14677d735aaf26a636fca047
Olly Headey <olly@headey.org>
2024-12-05 11:24:43 +0000

    Fix admin screens

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,6 +2,12 @@ class Admin::PostsController < AdminController
   include Pagy::Backend
 
   def index
-    @pagy, @posts = pagy(Post.joins(:user).where(users: { discarded_at: nil }).includes(:user).order(published_at: :desc), limit: 15)
+    @pagy, @posts = pagy(
+      Post.joins(blog: :user)
+          .where(users: { discarded_at: nil })
+          .includes(blog: :user)
+          .order(published_at: :desc),
+      limit: 15
+    )
   end
 end
--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -2,9 +2,9 @@ class Admin::StatsController < AdminController
   include Pagy::Backend
 
   def index
-    @pagy, @users = pagy(User.select("users.*, COUNT(posts.id) AS posts_count")
+    @pagy, @blogs = pagy(Blog.select("blogs.*, COUNT(posts.id) AS posts_count")
                           .left_outer_joins(:posts)
-                          .group("users.id")
+                          .group("blogs.id")
                           .order(created_at: :desc), limit: 15)
   end
 end
