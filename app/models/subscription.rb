7f8266bb7e7f1ffc2fc65c175b32997632d82897
Olly Headey <olly@headey.org>
2024-12-06 09:36:15 +0000

    Show number of active subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :user
 
+  scope :active, -> { where(cancelled_at: nil).where("next_billed_at > ?", Time.current) }
+
   def self.price
     "20"
   end
