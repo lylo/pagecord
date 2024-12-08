65a0e974635528a6ca579489c7b738de49e25336
Olly Headey <olly@headey.org>
2024-12-08 20:21:14 +0000

    Bump limit of custom domain changes

--- app/jobs/add_custom_domain_job.rb
+++ app/jobs/add_custom_domain_job.rb
@@ -6,7 +6,7 @@ class AddCustomDomainJob < ApplicationJob
 
     domain_changes_in_the_past_year = blog.custom_domain_changes.where("created_at > ?", 1.year.ago).count
 
-    if domain_changes_in_the_past_year >= 5
+    if domain_changes_in_the_past_year >= 20
       raise "Domain change limit exceeded for blog #{blog.name}"
     else
       Rails.logger.info "Adding custom domain #{domain} for blog #{blog.name}"
