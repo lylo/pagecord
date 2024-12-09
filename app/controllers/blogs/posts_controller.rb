9a680a79723d86fb006d5e57064e0706664a3c8b
Olly Headey <olly@headey.org>
2024-12-09 14:40:45 +0000

    Disable post caching for now

--- app/controllers/blogs/posts_controller.rb
+++ app/controllers/blogs/posts_controller.rb
@@ -18,7 +18,7 @@ class Blogs::PostsController < ApplicationController
   def show
     @post = @blog.posts.find_by!(token: blog_params[:token])
 
-    fresh_when @post, public: true, etag: [ @post, @post.upvotes_count ], last_modified: @post.updated_at
+    fresh_when(etag: [ @post, @post.upvotes_count ], last_modified: @post.updated_at)
   end
 
   private
