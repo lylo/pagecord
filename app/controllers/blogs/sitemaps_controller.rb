664fc03968b03edce90e131e488d07f1eda2134d
Olly Headey <olly@headey.org>
2024-12-17 16:57:11 +0000

    Use etags in sitemap

--- app/controllers/blogs/sitemaps_controller.rb
+++ app/controllers/blogs/sitemaps_controller.rb
@@ -1,4 +1,8 @@
 class Blogs::SitemapsController < Blogs::BaseController
   def show
+    fresh_when(
+      etag: @blog.posts.maximum(:updated_at),
+      last_modified: @blog.posts.maximum(:updated_at),
+      public: true)
   end
 end
