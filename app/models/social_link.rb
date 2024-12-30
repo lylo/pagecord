c5d01ac93cfbd2356a52155db140f48424be29f8
Olly Headey <olly@headey.org>
2024-12-30 15:47:50 +0000

    Add additional social platforms

--- app/models/social_link.rb
+++ app/models/social_link.rb
@@ -1,7 +1,7 @@
 class SocialLink < ApplicationRecord
   belongs_to :blog
 
-  PLATFORMS = [ "X", "Instagram", "Bluesky", "Threads", "YouTube" ].sort
+  PLATFORMS = [ "Bluesky", "GitHub", "Instagram", "LinkedIn", "Spotify", "Threads", "TikTok", "X", "YouTube" ].sort
 
   validates :platform, presence: true, uniqueness: { scope: :blog_id }, inclusion: { in: PLATFORMS }
   validates :url, presence: true
