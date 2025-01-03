f9658e627262d271740828ef129af84b72f4d2de
Olly Headey <olly@headey.org>
2025-01-03 14:38:26 +0000

    Change placeholder for social links
    Test for adding new subscriber
    
    Email subscriber test

--- test/controllers/blogs/email_subscribers_controller_test.rb
+++ test/controllers/blogs/email_subscribers_controller_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class Blogs::EmailSubscribersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @blog = blogs(:joel)
+  end
+
+  test "should add new email subscriber" do
+    assert_difference("EmailSubscriber.count", 1) do
+      post email_subscribers_url(name: @blog.name), params: { blog_name: @blog.name, email_subscriber: { email: "test@example.com" } }, as: :turbo_stream
+    end
+
+    assert_response :success
+    assert_includes @response.body, "Thanks for subscribing"
+  end
+
+  test "should not add existing email subscriber" do
+    assert_no_difference("EmailSubscriber.count") do
+      post email_subscribers_url(name: @blog.name), params: { blog_name: @blog.name, email_subscriber: { email: @blog.email_subscribers.first.email } }, as: :turbo_stream
+    end
+
+    assert_response :success
+    assert_includes @response.body, "is already subscribed"
+  end
 end
--- test/models/email_subscriber_test.rb
+++ test/models/email_subscriber_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class EmailSubscriberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be unique for a given blog" do
+    blog = blogs(:joel)
+
+    assert_raises(ActiveRecord::RecordNotUnique) do
+      blog.email_subscribers.create!(email: blog.email_subscribers.first.email)
+    end
+
+    assert_difference("EmailSubscriber.count", 1) do
+      blogs(:vivian).email_subscribers.create!(email: blog.email_subscribers.first.email)
+    end
+  end
 end
