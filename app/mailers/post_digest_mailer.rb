2a8ee9fe550d87f8d04d855d539980c8051b45b1
Olly Headey <olly@headey.org>
2025-01-07 09:23:47 +0000

    Restrict outbound digest emails temporarily

--- app/mailers/post_digest_mailer.rb
+++ app/mailers/post_digest_mailer.rb
@@ -1,16 +1,29 @@
 class PostDigestMailer < ApplicationMailer
   layout "mailer_digest"
 
-  helper_method :post_url_for, :unsubscribe_path_for
+  helper_method :blog_url_for, :post_url_for, :unsubscribe_path_for
 
   def weekly_digest
     @digest = params[:digest]
     @subscriber = params[:subscriber]
 
-    mail(to: @subscriber.email, subject: "New posts from #{@subscriber.blog.display_name}")
+    allowed_email_addresses = ENV["ALLOWED_EMAIL_ADDRESS"]&.split(",") || []
+    send_email = !Rails.env.production? || allowed_email_addresses.include?(@subscriber.email)
+
+    if send_email
+      mail(to: @subscriber.email, subject: "New posts from #{@subscriber.blog.display_name}. #{Date.current.to_formatted_s(:long)}")
+    end
   end
 
   private
+
+    def blog_url_for(blog)
+      if blog.custom_domain?
+        custom_blog_posts_url(blog)
+      else
+        blog_posts_url(name: blog.name)
+      end
+    end
 
     def post_url_for(post)
       if post.blog.custom_domain?
