ed44c3937e3c25c7c90e03d89f3e10830b9406a9
Olly Headey <olly@headey.org>
2025-01-08 10:51:10 +0000

    Take advantage of new normalizes method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,15 @@ class User < ApplicationRecord
   include Discard::Model
   include Followable, Subscribable
 
-  before_validation :downcase_email
-
   has_one :blog, dependent: :destroy, inverse_of: :user
   has_many :access_requests, dependent: :destroy
 
   accepts_nested_attributes_for :blog
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  normalizes :email, with: -> { it.downcase.strip }
 
   def verify!
     self.update! verified: true
   end
-
-  private
-
-    def downcase_email
-      self.email = email.downcase.strip
-    end
 end
