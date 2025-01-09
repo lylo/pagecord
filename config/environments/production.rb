ed23cb88a35cd2fe7894dbbca676dd4007708e3f
Olly Headey <olly@headey.org>
2025-01-09 21:17:43 +0000

    WiP for R2 migration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :s3
+  config.active_storage.service = :cloudflare
   config.active_storage.delivery_method = :proxy
 
   # Mount Action Cable outside main process or domain.
