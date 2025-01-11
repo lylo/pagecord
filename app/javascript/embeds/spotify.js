4f854646626b7366e809662d038aa538fe76d0b6
Olly Headey <olly@headey.org>
2025-01-11 08:05:41 +0000

    Support spotify artist embeds

--- app/javascript/embeds/spotify.js
+++ app/javascript/embeds/spotify.js
@@ -3,13 +3,15 @@ import MediaSite from "media_site"
 class Spotify extends MediaSite {
   constructor() {
     super(
-      /https:\/\/open\.spotify\.com\/(track|album|playlist|episode|show)\/([a-zA-Z0-9]+)(\?.*)?/,
+      /https:\/\/open\.spotify\.com\/(track|album|artist|playlist|episode|show)\/([a-zA-Z0-9]+)(\?.*)?/,
 
       async (url) => {
         const match = url.match(this.regex)
         if (match) {
           const type = match[1]
           const id = match[2]
+          console.log(type)
+          console.log(id)
           return `https://open.spotify.com/embed/${type}/${id}`
         }
         return null
