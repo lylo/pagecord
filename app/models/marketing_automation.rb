2a090a61767e6571c9aff41d77178e0412b333ad
Olly Headey <olly@headey.org>
2025-01-12 16:13:59 +0000

    Send getting started email to everyone

--- app/models/marketing_automation.rb
+++ app/models/marketing_automation.rb
@@ -1,6 +1,6 @@
 class MarketingAutomation
   def self.send_getting_started_emails
-    users = User.kept.where(created_at: 2.days.ago..1.day.ago).where(marketing_consent: true)
+    users = User.kept.where(created_at: 2.days.ago..1.day.ago)
     users.find_each do |user|
       MarketingAutomation::GettingStartedEmailJob.perform_later(user.id)
     end
