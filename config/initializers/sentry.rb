a791634c7949e8dbe7371c9e08e7c5ec6d3a8373
Olly Headey <olly@headey.org>
2025-02-06 08:00:24 +0000

    Move sentry config to env var

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   Sentry.init do |config|
-    config.dsn = "https://1ec707f4464720240f1c0b02e7ca5693@o4506953782067200.ingest.us.sentry.io/4506953837051904"
+    config.dsn = ENV["SENTRY_DSN"]
     config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
     # Set traces_sample_rate to 1.0 to capture 100%
