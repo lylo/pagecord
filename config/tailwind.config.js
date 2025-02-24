344d60d8ebd780e1e6523336d61ad9628af09e82
Olly Headey <olly@headey.org>
2025-02-24 16:28:55 +0000

    Fix up animation keyframes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,24 +7,5 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
-
-  theme: {
-    extend: {
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
-        fadeOut: {
-          '0%': { opacity: 1 },
-          '100%': { opacity: 0 },
-        },
-      },
-      animation: {
-        fadeIn: 'fadeIn 0.3s ease-out',
-        fadeOut: 'fadeOut 0.3s ease-out forwards',
-      },
-    },
-  },
   plugins: []
 }
