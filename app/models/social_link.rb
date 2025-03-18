f472de2175ba72a00703365fc2e115cd4b298360
Olly Headey <olly@headey.org>
2025-03-18 13:10:09 +0000

    Add support for Mastodon social link

--- app/models/social_link.rb
+++ app/models/social_link.rb
@@ -1,7 +1,7 @@
 class SocialLink < ApplicationRecord
   belongs_to :blog
 
-  PLATFORMS = [ "Bluesky", "GitHub", "Instagram", "LinkedIn", "Spotify", "Threads", "TikTok", "Web", "X", "YouTube" ].sort
+  PLATFORMS = [ "Bluesky", "GitHub", "Instagram", "LinkedIn", "Mastodon", "Spotify", "Threads", "TikTok", "Web", "X", "YouTube" ].sort
 
   validates :platform, presence: true, uniqueness: { scope: :blog_id }, inclusion: { in: PLATFORMS }
   validates :url, presence: true
