7d67911006a6043f3fdd70520fd44f0a51752021
Olly Headey <olly@headey.org>
2025-05-05 15:59:54 +0100

    Link to the post in the reply email

--- app/mailers/reply_mailer.rb
+++ app/mailers/reply_mailer.rb
@@ -1,13 +1,11 @@
 class ReplyMailer < ApplicationMailer
-  include ApplicationHelper
-
-  helper :application
+  helper :routing
 
   def new_reply
     @reply = params[:reply]
     @post = @reply.post
 
-    subject = "New reply to your post: #{post_title(@post)}"
+    subject = "New reply to your post: #{@post.display_title}"
 
     mail(
       to: @post.blog.user.email,
