40dd5c8b85c8906bef782018708e17f3d424fc4c
Olly Headey <olly@headey.org>
2025-05-13 15:32:30 +0100

    Add email social link

--- app/models/social_link.rb
+++ app/models/social_link.rb
@@ -1,7 +1,7 @@
 class SocialLink < ApplicationRecord
   belongs_to :blog
 
-  PLATFORMS = [ "Bluesky", "GitHub", "Instagram", "LinkedIn", "Mastodon",
+  PLATFORMS = [ "Bluesky", "Email", "GitHub", "Instagram", "LinkedIn", "Mastodon",
                 "RSS", "Spotify", "Threads", "TikTok", "Web", "X", "YouTube" ].sort
 
   validates :platform, presence: true, uniqueness: { scope: :blog_id }, inclusion: { in: PLATFORMS }
@@ -10,9 +10,15 @@ class SocialLink < ApplicationRecord
 
   scope :mastodon, -> { where(platform: "Mastodon") }
 
+  def email?
+    platform == "Email"
+  end
+
   private
 
     def validate_url_format
+      return if email?
+
       uri = URI.parse(url)
       errors.add(:url, "must be HTTP or HTTPS") unless uri.scheme.in?(%w[http https])
     rescue URI::InvalidURIError
