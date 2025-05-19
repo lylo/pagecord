bfa68abae0284fc3c41d9982e4c04e326e4f205c
Olly Headey <olly@headey.org>
2025-05-19 09:46:46 +0100

    Enable themes for everyone

--- app/controllers/app/settings/appearance_controller.rb
+++ app/controllers/app/settings/appearance_controller.rb
@@ -17,14 +17,9 @@ class App::Settings::AppearanceController < AppController
     def appearance_params
       permitted_params = [
         :bio, :title, :layout,
+        :theme, :font, :width,
         social_links_attributes: [ :id, :platform, :url, :_destroy ]
       ]
-
-      if current_features.enabled?(:themes)
-        permitted_params << [
-          :theme, :font, :width
-        ]
-      end
 
       permitted_params << [
         :avatar,
