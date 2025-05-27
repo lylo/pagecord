fd38c85afb3008b6f92c4ce6210036fd2f153b86
Olly Headey <olly@headey.org>
2025-05-27 19:14:34 +0100

    Simplified custom domain logic

--- app/helpers/custom_domain_helper.rb
+++ app/helpers/custom_domain_helper.rb
@@ -1,15 +1,7 @@
 module CustomDomainHelper
   def custom_domain_request?
     default_host = Rails.application.config.x.domain
-    if Rails.env.production?
-      !request.host.include?(default_host)
-    elsif Rails.env.test?
-      !request.host.include?(default_host)
-    else
-      # In development, only consider it a custom domain if it's not our development domains
-      local_domain_pattern = /\A([^.]+\.)*localhost\z/
-      !request.host.match?(local_domain_pattern)
-    end
+    !request.host.include?(default_host)
   end
 
   def default_domain_request?
