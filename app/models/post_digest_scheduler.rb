bdf8ab89d90ec9905dc342313d3922015376c105
Olly Headey <olly@headey.org>
2025-05-29 15:19:15 +0100

    Generate a new post digest (and deliver the emails for it) every 2 seconds

--- app/models/post_digest_scheduler.rb
+++ app/models/post_digest_scheduler.rb
@@ -1,13 +1,13 @@
 class PostDigestScheduler
   def self.run
-    Blog.where(email_subscriptions_enabled: true).includes(:user).find_each(batch_size: 100) do |blog|
+    Blog.where(email_subscriptions_enabled: true).includes(:user).find_each(batch_size: 100).with_index do |blog, index|
       user = blog.user
       next unless user&.kept? && user.subscribed?
 
       Time.use_zone(user.timezone || "UTC") do
         if Time.current.hour == 8
           Rails.logger.info "Generating digest for blog #{blog.id} (#{blog.subdomain}) - local time: #{Time.current}"
-          GeneratePostDigestJob.perform_later(blog.id)
+          GeneratePostDigestJob.set(wait: index * 2.seconds).perform_later(blog.id)
         end
       end
     end
