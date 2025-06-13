089720f11155c0d8b313e476061b8b9030d4eba7
Olly Headey <olly@headey.org>
2025-06-13 20:31:07 +0100

    Cancel paddle subscription when user cancels

--- app/jobs/destroy_user_job.rb
+++ app/jobs/destroy_user_job.rb
@@ -5,6 +5,10 @@ class DestroyUserJob < ApplicationJob
     user = User.find(user_id)
     user.discard!
 
+    if user.subscription
+      PaddleApi.new.cancel_subscription(user.subscription.paddle_subscription_id)
+    end
+
     MarketingAutomation::DeleteContactJob.perform_later(user_id) if options[:spam]
   end
 end
