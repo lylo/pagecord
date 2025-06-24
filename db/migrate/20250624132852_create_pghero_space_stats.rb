67904592ad52889dd03a688ce5ed14625e58f150
Olly Headey <olly@headey.org>
2025-06-24 15:04:50 +0100

    Rubocop

--- db/migrate/20250624132852_create_pghero_space_stats.rb
+++ db/migrate/20250624132852_create_pghero_space_stats.rb
@@ -8,6 +8,6 @@ class CreatePgheroSpaceStats < ActiveRecord::Migration[8.1]
       t.timestamp :captured_at
     end
 
-    add_index :pghero_space_stats, [:database, :captured_at]
+    add_index :pghero_space_stats, [ :database, :captured_at ]
   end
 end
