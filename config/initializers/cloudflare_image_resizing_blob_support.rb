c13a0031c0e22cdd04cfb3a8e83b2ceaddb98793
Olly Headey <olly@headey.org>
2025-06-27 13:02:05 +0100

    Add ActionText::Attachment support for image resizing
    Extends CloudflareImageResizingBlobSupport to handle ActionText::Attachment objects, enabling image resizing and preload functionality for attachments embedded via ActionText.

--- config/initializers/cloudflare_image_resizing_blob_support.rb
+++ config/initializers/cloudflare_image_resizing_blob_support.rb
@@ -2,6 +2,8 @@ module CloudflareImageResizingBlobSupport
   def resizable?(image)
     if image.is_a?(ActiveStorage::Blob)
       image.content_type.in?(CloudflareImageResizing::Helper::RESIZABLE_CONTENT_TYPES)
+    elsif image.is_a?(ActionText::Attachment)
+      image.attachable.content_type.in?(CloudflareImageResizing::Helper::RESIZABLE_CONTENT_TYPES)
     else
       super
     end
@@ -11,6 +13,24 @@ module CloudflareImageResizingBlobSupport
     if image.is_a?(ActiveStorage::Blob)
       preload = options.delete(:preload)
       path = rails_public_blob_url(image)
+
+      if resizable?(image)
+        if ::Rails.application.config.cloudflare_image_resizing.enabled
+          path = "/" + path unless path.starts_with?("/")
+          path = "/cdn-cgi/image/" + options.to_param.tr("&", ",") + path
+        end
+
+        if preload
+          content_for(:cloudflare_image_resizing_preload) do
+            preload_link_tag(path, as: :image)
+          end
+        end
+      end
+
+      path
+    elsif image.is_a?(ActionText::Attachment)
+      preload = options.delete(:preload)
+      path = rails_public_blob_url(image.attachable)
 
       if resizable?(image)
         if ::Rails.application.config.cloudflare_image_resizing.enabled
