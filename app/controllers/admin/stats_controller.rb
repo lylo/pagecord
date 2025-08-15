3ca077cefd37a4912301d91c063d6c4c6c2264cf
Olly Headey <olly@headey.org>
2025-08-15 09:44:37 +0100

    Add basic filters to stats page

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -7,11 +7,21 @@ class Admin::StatsController < AdminController
     blogs = Blog.select("blogs.*, COUNT(posts.id) AS posts_count")
                 .left_outer_joins(:posts)
                 .joins(:user)
+                .left_outer_joins(user: :subscription)
                 .group("blogs.id")
                 .order(created_at: :desc)
 
     if params[:search].present?
       blogs = blogs.where("blogs.subdomain ILIKE ? OR users.email ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
+    if params[:status].present?
+      case params[:status]
+      when "paid"
+        blogs = blogs.where("subscriptions.complimentary = ? AND subscriptions.cancelled_at IS NULL AND subscriptions.next_billed_at > ?", false, Time.current)
+      when "comped"
+        blogs = blogs.where("subscriptions.complimentary = ?", true)
+      end
     end
 
     @pagy, @blogs = pagy(blogs, limit: 15)
