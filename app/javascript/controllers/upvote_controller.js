ac7e00bfabe2bd7a2b82f92feacd1ca1b62e6325
Olly Headey <olly@headey.org>
2025-08-31 09:26:37 +0100

    Submit upvote request immediately

--- app/javascript/controllers/upvote_controller.js
+++ app/javascript/controllers/upvote_controller.js
@@ -5,16 +5,11 @@ export default class extends Controller {
   static targets = ["heart"]
 
   pulse(event) {
-    // stop immediate submit
-    event.preventDefault()
-
     const heart = this.heartTarget
     heart.classList.add("animate-pulse-grow", "text-red-500")
 
-    // after 1s remove animation + submit the form
     setTimeout(() => {
       heart.classList.remove("animate-pulse-grow", "text-red-500")
-      this.element.closest("form").requestSubmit()
     }, 500)
   }
 }
