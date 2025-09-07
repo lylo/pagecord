c8c8e7fc94576ed391bc2a21b62465ccdab7a82c
Olly Headey <olly@headey.org>
2025-09-07 22:16:32 +0100

    Strip figure tags but preserve images within

--- app/models/html/sanitize.rb
+++ app/models/html/sanitize.rb
@@ -1,7 +1,7 @@
 module Html
   class Sanitize < Transformation
     ALLOWED_TAGS = %w[
-      a abbr figure figcaption action-text-attachment b blockquote br cite code del div em h1 h2 h3 h4 h5 h6 hr i img li mark ol p pre s span strike strong u ul
+      a abbr action-text-attachment b blockquote br cite code del div em h1 h2 h3 h4 h5 h6 hr i img li mark ol p pre s span strike strong u ul
     ]
     ALLOWED_ATTRIBUTES = %w[
       href src sgid url content-type name filename
@@ -10,8 +10,8 @@ module Html
     def transform(html)
       document = Nokogiri::HTML::DocumentFragment.parse(html)
       document.search("img").each do |img|
-        # Preserve images inside ActionText attachments
-        if img.ancestors("action-text-attachment").any?
+        # Preserve images inside figures (HEY.com)
+        if img.ancestors("figure").any?
         elsif img["pagecord"] == "true"
           img.remove_attribute("pagecord")
         else
