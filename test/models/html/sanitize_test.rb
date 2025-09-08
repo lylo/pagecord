877e0cb0121d13f0ab807c53bc219e09aa965db2
Olly Headey <olly@headey.org>
2025-09-08 16:23:05 +0100

    Remove dead test

--- test/models/html/sanitize_test.rb
+++ test/models/html/sanitize_test.rb
@@ -16,16 +16,4 @@ class Html::SanitizeTest < ActiveSupport::TestCase
     HTML
     assert_equal expected_html.strip, transformed_html
   end
-
-  test "should preserve lexxy syntax highlighting with single quotes" do
-    html = "<pre data-language='ruby'>puts 'hello'</pre>"
-
-    transformed_html = Html::Sanitize.new.transform(html)
-    
-    puts "Input HTML: #{html}"
-    puts "Output HTML: #{transformed_html}"
-    
-    assert_includes transformed_html, 'data-language'
-    assert_includes transformed_html, 'ruby'
-  end
 end
