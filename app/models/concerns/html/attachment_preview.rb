f4a83d9d7b7318f9eb01629047a25650386a3bb3
Olly Headey <olly@headey.org>
2025-09-19 14:23:14 +0100

    Leverage ActionText helper, don't hard-code the node

--- app/models/concerns/html/attachment_preview.rb
+++ app/models/concerns/html/attachment_preview.rb
@@ -2,13 +2,6 @@ module Html::AttachmentPreview
   extend ActiveSupport::Concern
 
   def attachment_preview_node(blob, url, original)
-    %Q(
-      <action-text-attachment sgid="#{blob.attachable_sgid}" content-type="#{original.content_type}"
-        filename="#{original.filename}" filesize="#{blob.byte_size}" previewable="true" url="#{url}">
-        <figure class="attachment attachment--preview attachment--#{blob.filename.extension}">
-          <img alt="" src="#{url}">
-        </figure>
-      </action-text-attachment>
-    )
+    ActionText::Attachment.from_attachable(blob).to_html
   end
 end
