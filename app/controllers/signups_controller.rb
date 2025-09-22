4e84ad19b7978696f7d93a7d985f333e847fe9b0
Olly Headey <olly@headey.org>
2025-09-22 15:48:37 +0100

    Rubocop

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -25,9 +25,9 @@ class SignupsController < ApplicationController
     end
 
     @user = User.new(user_params)
-    
-    # New users should get Lexxy if configured 
-    @user.features = ["lexxy"] if ENV["LEXXY_FOR_NEW_USERS"]
+
+    # New users should get Lexxy if configured
+    @user.features = [ "lexxy" ] if ENV["LEXXY_FOR_NEW_USERS"]
 
     if signup_from_allowed_timezone && @user.save
       AccountVerificationMailer.with(user: @user).verify.deliver_later
