e8b0bc2e9a713cbd4ab229708c8c2717da47a724
Olly Headey <olly@headey.org>
2025-09-22 15:00:19 +0100

    Feature flag to give new users Lexxy

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -25,6 +25,10 @@ class SignupsController < ApplicationController
     end
 
     @user = User.new(user_params)
+    
+    # New users should get Lexxy if configured 
+    @user.features = ["lexxy"] if ENV["LEXXY_FOR_NEW_USERS"]
+
     if signup_from_allowed_timezone && @user.save
       AccountVerificationMailer.with(user: @user).verify.deliver_later
 
