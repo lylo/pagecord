98b548f1852d26bb12f338ee87631f847ce3cbc8
Olly Headey <olly@headey.org>
2025-09-23 14:47:24 +0100

    Rate limit the custom domain verification action

--- app/controllers/custom_domains_controller.rb
+++ app/controllers/custom_domains_controller.rb
@@ -1,6 +1,8 @@
 class CustomDomainsController < ApplicationController
   skip_before_action :verify_authenticity_token, :domain_check
 
+  rate_limit to: 10, within: 1.minute, only: :verify
+
   def verify
     domain = params[:domain]&.downcase
     return head :bad_request if domain.blank?
