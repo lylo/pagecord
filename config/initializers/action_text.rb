a37a069273f8e7a3e88d5f30a00614a928fcd011
Olly Headey <olly@headey.org>
2025-10-06 11:35:44 +0100

    Don't filter data-language or data-highlight-language

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -1,5 +1,5 @@
 Rails.application.config.after_initialize do
   base_attributes = ActionText::ContentHelper.sanitizer.class.allowed_attributes
   attachment_attributes = ActionText::Attachment::ATTRIBUTES
-  ActionText::ContentHelper.allowed_attributes = base_attributes + attachment_attributes + [ "data-lightbox-full-url" ]
+  ActionText::ContentHelper.allowed_attributes = base_attributes + attachment_attributes + [ "data-lightbox-full-url", "data-language", "data-highlight-language" ]
 end
