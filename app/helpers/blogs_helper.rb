63c2ae6d17e244bfc467e9ba911f00f9ee7758be
Olly Headey <olly@headey.org>
2025-10-22 09:16:08 +0100

    Whitespace issue

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -25,7 +25,7 @@ module BlogsHelper
         blog_title(@post.blog)
       elsif @post.title&.present?
         "#{@post.title} - #{@post.blog.display_name}"
-      else 
+      else
         "#{blog_title(@post.blog)} - #{@post.published_at_in_user_timezone.to_formatted_s(:long)}"
       end
     elsif @blog
