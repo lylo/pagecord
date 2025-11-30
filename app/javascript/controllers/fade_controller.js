9f27c467c5ec216c63889a543f4035652ee83365
Olly Headey <olly@headey.org>
2025-11-30 20:41:47 +0000

    Render the flash error message for longer to allow the user to read and digest

--- app/javascript/controllers/fade_controller.js
+++ app/javascript/controllers/fade_controller.js
@@ -1,6 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = {
+    duration: { type: Number, default: 3000 }
+  }
+
   connect() {
     this.element.classList.remove('hidden')
     this.element.classList.add('animate-fadeIn')
@@ -12,6 +16,6 @@ export default class extends Controller {
         this.element.remove(); // Remove the element from the DOM
       }, { once: true });
 
-    }, 3000)
+    }, this.durationValue)
   }
 }
