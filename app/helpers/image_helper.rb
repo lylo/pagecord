2e168d9ea336990ed90b1b197e2d09f21de8d551
Olly Headey <olly@headey.org>
2025-12-04 12:39:07 +0000

    Revert unnecessary change to interface of resize_image_url

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,12 +1,12 @@
 module ImageHelper
-  def resized_image_url(blob, width:, height:, crop: false, format: :webp)
+  def resized_image_url(blob, width:, height:, crop: false)
     cloudflare_enabled = Rails.configuration.x.cloudflare_image_resizing.present?
 
     if cloudflare_enabled
       public_url = rails_public_blob_url(blob)
       base_url = "https://#{Rails.configuration.x.domain}"
 
-      options = "width=#{width},height=#{height},format=#{format},quality=90"
+      options = "width=#{width},height=#{height},format=webp,quality=90"
       options += ",fit=cover,gravity=auto" if crop
 
       "#{base_url}/cdn-cgi/image/#{options}/#{public_url}"
@@ -14,7 +14,7 @@ module ImageHelper
       resize_method = crop ? :resize_to_fill : :resize_to_limit
       variant = blob.variant(
         resize_method => [ width, height ],
-        format: format,
+        format: :webp,
         saver: { quality: 90, strip: true, compression: 6 }
       )
       rails_public_blob_url(variant)
