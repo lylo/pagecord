9d0fdad680460438045f8f3d54c64becf4c2810d
Olly Headey <olly@headey.org>
2025-12-08 15:06:59 +0000

    Set up feature toggle to test Resend on a per-blog basis for ReplyMailer only

--- app/mailers/reply_mailer.rb
+++ app/mailers/reply_mailer.rb
@@ -4,13 +4,22 @@ class ReplyMailer < MailpaceMailer
   def new_reply
     @reply = params[:reply]
     @post = @reply.post
+    @blog = @post.blog
 
     subject = "Re: #{@post.display_title}"
 
     mail(
-      to: @post.blog.user.email,
+      to: @blog.user.email,
       subject: subject,
       reply_to: @reply.email
-    )
+    ).tap { |m| use_resend(m) if Rails.features.for(blog: @blog).enabled?(:resend) }
   end
+
+  private
+
+    def use_resend(message)
+      return unless Rails.env.production?
+      message.delivery_method(:resend)
+      message.from = "Pagecord <no-reply@remailer.pagecord.com>"
+    end
 end
--- config/features.rb
+++ config/features.rb
@@ -8,6 +8,6 @@ feature :custom_css do |blog: nil|
   blog&.features.include?("custom_css")
 end
 
-feature :dynamic_open_graph do |blog: nil|
-  blog&.features.include?("dynamic_open_graph")
+feature :resend do |blog: nil|
+  blog&.features.include?("resend")
 end
