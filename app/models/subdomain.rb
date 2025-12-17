f4df681c8afdbc1909af51273d1dffda8c2cb3aa
Olly Headey <olly@headey.org>
2025-12-17 15:41:23 +0000

    Reserve guide subdomain

--- app/models/subdomain.rb
+++ app/models/subdomain.rb
@@ -1,7 +1,7 @@
 class Subdomain
   RESERVED = %w[ about admin app api careers community contact discover faq feed
-                 help images jobs login og pagecord privacy pricing rails rss
-                 sidekiq signup support terms ]
+                 guide help images jobs login og pagecord privacy pricing rails
+                 rss sidekiq signup support terms ]
 
   FORMAT_REGEX = /\A[a-zA-Z0-9]+\z/
   MIN_LENGTH = 3
