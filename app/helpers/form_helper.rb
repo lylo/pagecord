efdb997d6b5fb3c52973dbc350ecfa968c901572
Olly Headey <olly@headey.org>
2026-01-01 14:05:49 +0000

    Rubocop

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -4,7 +4,7 @@ module FormHelper
   end
 
   def styled_text_field(form, field, options = {})
-    final_class = ["form-field", options[:class]].compact.join(" ")
+    final_class = [ "form-field", options[:class] ].compact.join(" ")
     form.text_field field, options.merge(class: final_class)
   end
 end
