c46625d463754e3e6376c86b35f83ed8e2f3a7a7
Olly Headey <olly@headey.org>
2026-01-26 10:41:49 +0000

    Add flash message when export rate limit is exceeded

--- app/controllers/app/settings/exports_controller.rb
+++ app/controllers/app/settings/exports_controller.rb
@@ -1,5 +1,5 @@
 class App::Settings::ExportsController < AppController
-  rate_limit to: 5, within: 1.day, only: :create
+  rate_limit to: 5, within: 1.day, only: :create, with: :export_limit_reached
 
   def index
     @exports = @blog.exports
@@ -25,5 +25,9 @@ class App::Settings::ExportsController < AppController
 
     def export_params
       params.fetch(:blog_export, {}).permit(:format)
+    end
+
+    def export_limit_reached
+      redirect_to app_settings_exports_path, alert: "You've reached the limit of 5 exports per day. Please try again tomorrow."
     end
 end
