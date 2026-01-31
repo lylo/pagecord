e8fa76b47c2ada6c8d99c0258e03ecb06d3f99b6
Olly Headey <olly@headey.org>
2026-01-31 11:36:06 +0000

    Add message length validation to contact messages and post replies

--- app/models/blog/contact_message.rb
+++ app/models/blog/contact_message.rb
@@ -5,5 +5,5 @@ class Blog::ContactMessage < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :message, presence: true
+  validates :message, presence: true, length: { maximum: 8.kilobytes }
 end
