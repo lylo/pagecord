8a9d8e930bfeb8f0807c04993af335520b1b4dae
Olly Headey <olly@headey.org>
2026-02-05 11:50:40 +0000

    Add individual mailer preview and ignore Brakeman html_safe warning
    Fix digest mailer preview to not destroy existing digests, add
    individual email preview method. Add brakeman.ignore for the
    intentional html_safe on ActionText content in email partial.

--- test/mailers/previews/post_digest_mailer_preview.rb
+++ test/mailers/previews/post_digest_mailer_preview.rb
@@ -1,9 +1,30 @@
 # Preview all emails at http://localhost:3000/rails/mailers/post_digest_mailer
 class PostDigestMailerPreview < ActionMailer::Preview
   def weekly_digest
-    PostDigest.destroy_all
     blog = Blog.find_by(subdomain: "joel")
-    digest = PostDigest.generate_for(blog)
-    PostDigestMailer.with(digest: digest, subscriber: digest.blog.email_subscribers.first).weekly_digest
+    digest = blog.post_digests.weekly_digest.last || create_weekly_digest(blog)
+    PostDigestMailer.with(digest: digest, subscriber: blog.email_subscribers.first).weekly_digest
   end
+
+  def individual
+    blog = Blog.find_by(subdomain: "joel")
+    digest = blog.post_digests.individual.last || create_individual_digest(blog)
+    PostDigestMailer.with(digest: digest, subscriber: blog.email_subscribers.first).individual
+  end
+
+  private
+
+    def create_weekly_digest(blog)
+      posts = blog.posts.visible.order(published_at: :desc).limit(3)
+      digest = PostDigest.create!(blog: blog, kind: :weekly_digest)
+      posts.each { |post| digest.digest_posts.create!(post: post) }
+      digest
+    end
+
+    def create_individual_digest(blog)
+      post = blog.posts.visible.order(published_at: :desc).first
+      digest = PostDigest.create!(blog: blog, kind: :individual)
+      digest.digest_posts.create!(post: post)
+      digest
+    end
 end
