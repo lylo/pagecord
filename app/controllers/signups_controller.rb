62ee80357a1cc7e815190764fea7cebbad9c8d87
Olly Headey <olly@headey.org>
2026-02-09 14:27:02 +0000

    Remove redundant code. Everyone now gets Lexxy

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -26,9 +26,6 @@ class SignupsController < ApplicationController
 
     @user = User.new(user_params)
 
-    # New users should get Lexxy if configured
-    @user.blog.features = [ "lexxy" ] if ENV["LEXXY_FOR_NEW_USERS"]
-
     if signup_from_allowed_timezone && @user.save
       AccountVerificationMailer.with(user: @user).verify.deliver_later
 
