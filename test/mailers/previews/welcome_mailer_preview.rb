76cd74c0cd24f0083ade8a94195ab1c305d1f9c3
Olly Headey <olly@headey.org>
2026-02-24 09:06:02 +0000

    Increase annual price from $29 to $39, discounted countries from $19 to $25
    Update Paddle price ID for annual plan. Remove outdated "Save 40%"
    claim from plan cards. Existing subscribers are grandfathered at their
    current rate via stored unit_price.

--- test/mailers/previews/welcome_mailer_preview.rb
+++ test/mailers/previews/welcome_mailer_preview.rb
@@ -2,7 +2,7 @@ class WelcomeMailerPreview < ActionMailer::Preview
   def welcome_email
     @user = User.first
     @blog = @user.blog
-    WelcomeMailer.with(user: @user, blog: @blog, price: "29").welcome_email
+    WelcomeMailer.with(user: @user, blog: @blog, price: "39").welcome_email
   end
 
   def unengaged_follow_up
