cf5173343c4ba95649e97c71becac52a37bcb163
Olly Headey <olly@headey.org>
2026-03-03 17:30:45 +0000

    Improve tag filter notice with RSS feed link
    Redesign the tag filter banner as a compact notice with the tag name
    and two inline links: "Show all posts" and "RSS feed for this tag".

--- test/controllers/blogs/posts_controller_test.rb
+++ test/controllers/blogs/posts_controller_test.rb
@@ -718,8 +718,9 @@ class Blogs::PostsControllerTest < ActionDispatch::IntegrationTest
     get blog_posts_path(tag: "rails")
 
     assert_response :success
-    assert_select "div", text: /Showing posts tagged with "rails"/
-    assert_select "a[href='#{blog_posts_list_path}']", text: "Show all posts"
+    assert_select "p", text: /Tagged with "rails"/
+    assert_select "a.tag-filter-clear[href='#{blog_posts_list_path}']", text: "Show all posts"
+    assert_select "a.tag-filter-rss", text: "RSS feed for this tag"
   end
 
   test "should show no posts message when tag has no matches" do
