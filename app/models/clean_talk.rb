f2b2f9e659695cb4d056dd8ad2561e1ae85ae35e
Olly Headey <olly@headey.org>
2026-03-16 10:48:17 +0000

    Send page URL to CleanTalk via sender_info for spam log traceability

--- app/models/clean_talk.rb
+++ app/models/clean_talk.rb
@@ -5,14 +5,20 @@ class CleanTalk
   base_uri "https://moderate.cleantalk.org"
   default_timeout 5
 
-  def self.check_message(email:, nickname:, message:)
-    response = post("/api2.0", body: {
+  def self.check_message(email:, nickname:, message:, page_url: nil)
+    body = {
       method_name: "check_message",
       auth_key: ENV["CLEANTALK_AUTH_KEY"],
       sender_email: email,
       sender_nickname: nickname,
       message: message
-    }.to_json, headers: { "Content-Type" => "application/json" })
+    }
+
+    if page_url
+      body[:sender_info] = { page_url: page_url, REFERRER: page_url }.to_json
+    end
+
+    response = post("/api2.0", body: body.to_json, headers: { "Content-Type" => "application/json" })
 
     JSON.parse(response.body)
   end
