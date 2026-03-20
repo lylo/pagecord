52306b718c42a267ff48a0a1b9b02b6263c4d6ac
Olly Headey <olly@headey.org>
2026-03-20 12:15:41 +0000

    Add churning status badge and contextual date sorting to admin blogs

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -3,27 +3,26 @@ class Admin::BlogsController < AdminController
 
   def index
     @total_users = Blog.count
+    @date_column = helpers.blogs_date_column
 
-    blogs = Blog.select("blogs.*, COUNT(posts.id) AS posts_count")
+    blogs = Blog.select("blogs.*, COUNT(posts.id) AS posts_count, #{@date_column[:sql]} AS status_date")
                 .left_outer_joins(:posts)
                 .joins(:user)
                 .left_outer_joins(user: :subscription)
                 .group("blogs.id")
-                .order(created_at: :desc)
+                .order("#{@date_column[:sql]} DESC NULLS LAST")
 
     if params[:search].present?
       blogs = blogs.where("blogs.subdomain ILIKE ? OR users.email ILIKE ? OR subscriptions.paddle_customer_id ILIKE ? OR subscriptions.paddle_subscription_id ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
     end
 
-    if params[:status].present?
-      case params[:status]
-      when "paid"
-        blogs = blogs.where("subscriptions.plan IN (?) AND subscriptions.cancelled_at IS NULL AND subscriptions.next_billed_at > ?", [ "annual", "monthly" ], Time.current)
-      when "comped"
-        blogs = blogs.where("subscriptions.plan = ?", "complimentary")
-      when "cancelled"
-        blogs = blogs.where.not(subscriptions: { cancelled_at: nil })
-      end
+    case params[:status]
+    when "paid"
+      blogs = blogs.where("subscriptions.plan IN (?) AND subscriptions.cancelled_at IS NULL AND subscriptions.next_billed_at > ?", [ "annual", "monthly" ], Time.current)
+    when "comped"
+      blogs = blogs.where("subscriptions.plan = ?", "complimentary")
+    when "churning"
+      blogs = blogs.where.not(subscriptions: { cancelled_at: nil }).where("subscriptions.next_billed_at > ?", Time.current)
     end
 
     @pagy, @blogs = pagy(blogs, limit: 15)
