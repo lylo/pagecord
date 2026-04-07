8961b6f7f9a34070ea5bf10064215b51a8b95a35
Olly Headey <olly@headey.org>
2026-04-07 19:41:49 +0100

    Add Spotlight publication buffer

--- app/controllers/home/spotlight_controller.rb
+++ app/controllers/home/spotlight_controller.rb
@@ -1,4 +1,7 @@
 class Home::SpotlightController < ApplicationController
+  CACHE_DURATION = 15.minutes
+  ELIGIBILITY_DELAY = 15.minutes
+
   include RoutingHelper
   layout "home"
   before_action -> { redirect_to root_path unless Current.user&.admin? } # TODO: remove before launch
@@ -6,20 +9,38 @@ class Home::SpotlightController < ApplicationController
   def show
     @tab = params[:tab] == "recent" ? "recent" : "trending"
 
-    @posts = if @tab == "recent"
-      Rails.cache.fetch("public_spotlight_recent_posts", expires_in: 1.hour) do
-        Post.visible.posts
-          .joins(blog: :user)
-          .where(blogs: { allow_search_indexing: true })
-          .where(users: { discarded_at: nil })
+    @posts = @tab == "recent" ? recent_posts : trending_posts
+  end
+
+  private
+
+    def recent_posts
+      Rails.cache.fetch("public_spotlight_recent_posts", expires_in: CACHE_DURATION) do
+        spotlight_posts_scope
+          .where("published_at <= ?", spotlight_cutoff_time)
           .order(published_at: :desc)
           .limit(20)
           .includes(:blog)
       end
-    else
-      Rails.cache.fetch("public_spotlight_trending_posts", expires_in: 1.day) do
-        Analytics::Trending.new.top_posts(limit: 20)
+    end
+
+    def trending_posts
+      Rails.cache.fetch("public_spotlight_trending_posts", expires_in: CACHE_DURATION) do
+        # Delay Spotlight eligibility briefly so new posts have a short privacy buffer.
+        Analytics::Trending.new.top_posts(limit: 100)
+          .select { |item| item[:post].published_at <= spotlight_cutoff_time }
+          .first(20)
       end
     end
-  end
+
+    def spotlight_posts_scope
+      Post.visible.posts
+        .joins(blog: :user)
+        .where(blogs: { allow_search_indexing: true })
+        .where(users: { discarded_at: nil })
+    end
+
+    def spotlight_cutoff_time
+      ELIGIBILITY_DELAY.ago
+    end
 end
--- test/controllers/home/spotlight_controller_test.rb
+++ test/controllers/home/spotlight_controller_test.rb
@@ -4,9 +4,14 @@ class Home::SpotlightControllerTest < ActionDispatch::IntegrationTest
   include AuthenticatedTest
 
   setup do
+    Rails.cache.clear
     host! Rails.application.config.x.domain
     @admin = users(:joel)
     login_as @admin
+  end
+
+  teardown do
+    Rails.cache.clear
   end
 
   test "should show spotlight page" do
@@ -48,6 +53,31 @@ class Home::SpotlightControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_no_match user.blog.subdomain, response.body
+  end
+
+  test "excludes posts published within the last 15 minutes from recent" do
+    travel_to Time.zone.parse("2026-04-07 12:00:00") do
+      post = posts(:one)
+      post.update_column(:published_at, 5.minutes.ago)
+
+      get spotlight_path(tab: "recent")
+
+      assert_response :success
+      assert_no_match post.slug, response.body
+    end
+  end
+
+  test "excludes posts published within the last 15 minutes from trending" do
+    travel_to Time.zone.parse("2026-04-07 12:00:00") do
+      post = posts(:three)
+      post.update_columns(published_at: 5.minutes.ago, upvotes_count: 25)
+      PageView.create!(blog: post.blog, post: post, viewed_at: 5.minutes.ago, is_unique: true, visitor_hash: "fresh-post")
+
+      get spotlight_path
+
+      assert_response :success
+      assert_no_match post.slug, response.body
+    end
   end
 
   test "redirects non-admin users" do
