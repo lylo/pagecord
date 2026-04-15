aed8bb25f9940924f8e85543fa8f7ac600eea566
Olly Headey <olly@headey.org>
2026-04-15 09:52:55 +0100

    Replace CloudflareRoutable tests with basic mailer assertions

--- test/mailers/account_verification_mailer_test.rb
+++ test/mailers/account_verification_mailer_test.rb
@@ -5,39 +5,15 @@ class AccountVerificationMailerTest < ActionMailer::TestCase
     @user = users(:joel)
   end
 
-  test "login does not route to cloudflare without feature" do
-    stub_cloudflare_env do
-      email = AccountVerificationMailer.with(user: @user).login
-      refute_instance_of CloudflareEmail::DeliveryMethod, email.delivery_method
-    end
+  test "verify sends to user email" do
+    email = AccountVerificationMailer.with(user: @user).verify
+    assert_equal [ @user.email ], email.to
+    assert_equal "Verify your Pagecord email address", email.subject
   end
 
-  test "login routes to cloudflare when feature is enabled" do
-    @user.blog.update!(features: [ "cloudflare_email" ])
-
-    stub_cloudflare_env do
-      email = AccountVerificationMailer.with(user: @user).login
-      assert_instance_of CloudflareEmail::DeliveryMethod, email.delivery_method
-    end
+  test "login sends to user email" do
+    email = AccountVerificationMailer.with(user: @user).login
+    assert_equal [ @user.email ], email.to
+    assert_equal "Log into your Pagecord account", email.subject
   end
-
-  test "verify does not route to cloudflare even with feature enabled" do
-    @user.blog.update!(features: [ "cloudflare_email" ])
-
-    stub_cloudflare_env do
-      email = AccountVerificationMailer.with(user: @user).verify
-      refute_instance_of CloudflareEmail::DeliveryMethod, email.delivery_method
-    end
-  end
-
-  private
-
-    def stub_cloudflare_env
-      ENV["CLOUDFLARE_EMAIL_API_TOKEN"] = "test-token"
-      ENV["CLOUDFLARE_ACCOUNT_ID"] = "test-account"
-      yield
-    ensure
-      ENV.delete("CLOUDFLARE_EMAIL_API_TOKEN")
-      ENV.delete("CLOUDFLARE_ACCOUNT_ID")
-    end
 end
