63a4eab6a1aed4d5383a328592938dd3a6a8844a
Olly Headey <olly@headey.org>
2026-05-30 14:04:49 +0100

    Cache Bandcamp embed lookups

--- app/controllers/api/embeds_controller.rb
+++ app/controllers/api/embeds_controller.rb
@@ -6,6 +6,8 @@ class Api::EmbedsController < ApplicationController
 
   def bandcamp
     url = params[:url]
+    return render json: { error: "Invalid Bandcamp URL" }, status: :unprocessable_entity unless bandcamp_url?(url)
+
     embed_url = og_video_attribute(url)
 
     if embed_url
@@ -19,9 +21,18 @@ class Api::EmbedsController < ApplicationController
 
   private
 
+    def bandcamp_url?(url)
+      uri = URI.parse(url)
+      uri.is_a?(URI::HTTPS) && (uri.host == "bandcamp.com" || uri.host&.end_with?(".bandcamp.com"))
+    rescue URI::InvalidURIError
+      false
+    end
+
     def og_video_attribute(url)
-      doc = Nokogiri::HTML(URI.open(url))
-      meta_tag = doc.at("meta[property=\"og:video\"]")
-      meta_tag&.attr("content")
+      Rails.cache.fetch([ "bandcamp_embed", url ], expires_in: 7.days, race_condition_ttl: 10.seconds) do
+        doc = Nokogiri::HTML(URI.open(url, open_timeout: 2, read_timeout: 3))
+        meta_tag = doc.at("meta[property=\"og:video\"]")
+        meta_tag&.attr("content")
+      end
     end
 end
