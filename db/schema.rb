66148be36f4a11cccc3016cb02618150ed916d28
Olly Headey <olly@headey.org>
2026-06-02 11:49:04 +0100

    Remove blog feature flags column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.2].define(version: 2026_06_02_120000) do
+ActiveRecord::Schema[8.2].define(version: 2026_06_02_130000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -110,7 +110,6 @@ ActiveRecord::Schema[8.2].define(version: 2026_06_02_120000) do
     t.datetime "discarded_at"
     t.integer "email_delivery_mode", default: 0, null: false
     t.boolean "email_subscriptions_enabled", default: true, null: false
-    t.string "features", default: [], array: true
     t.string "fediverse_author_attribution"
     t.string "font", default: "sans", null: false
     t.string "google_site_verification"
