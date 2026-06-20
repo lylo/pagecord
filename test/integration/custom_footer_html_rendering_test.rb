e8132358321d15d2e70b655ff49cfd0fa487aa37
Olly Headey <olly@headey.org>
2026-06-20 09:26:55 +0100

    Revert "Allow safe inline styles in custom footers"
    This reverts commit b5cf47357e90a74422b9b02729cf74cc83ab6d8d.

--- test/integration/custom_footer_html_rendering_test.rb
+++ test/integration/custom_footer_html_rendering_test.rb
@@ -18,16 +18,6 @@ class CustomFooterHtmlRenderingTest < ActionDispatch::IntegrationTest
     assert response.body.index('class="custom-footer"') < response.body.index('id="brand"')
   end
 
-  test "renders safe inline styles in custom footer" do
-    @blog.update!(custom_footer_html: '<a href="https://www.buymeacoffee.com/heyolly" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-green.png" alt="Buy Me a Coffee" style="height: 60px !important;width: 217px !important;" ></a>')
-    post = posts(:one)
-
-    get blog_post_url(subdomain: @blog.subdomain, slug: post.slug)
-
-    assert_response :success
-    assert_select ".custom-footer a[href='https://www.buymeacoffee.com/heyolly'] img[style='height:60px !important;width:217px !important;']"
-  end
-
   test "does not render custom footer for non-premium users" do
     user = users(:vivian)
     user.blog.update!(custom_footer_html: '<a href="https://example.com">Example</a>')
