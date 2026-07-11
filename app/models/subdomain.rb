54ad2a6026469d9c58a91d5d7fa2ffd9c3926ad8
Olly Headey <olly@headey.org>
2026-07-11 20:01:36 +0100

    Reserve discuss and social subdomains

--- app/models/subdomain.rb
+++ app/models/subdomain.rb
@@ -1,11 +1,11 @@
 class Subdomain
   RESERVED = %w[ about abuse account accounts admin app api archive assets auth
                  billing blog careers cdn cfmail checkout community contact dev
-                 discover docs domains email faq feed files guide help home images
+                 discover discuss docs domains email faq feed files guide help home images
                  inbound jobs login mail mailer media metrics monitoring mx news
                  newsletters notifications oauth og pagecord pm-bounces post press
                  pricing privacy proxy rails rss security settings shuffle sidekiq
-                 signup smtp staging static status storage support terms test
+                 signup smtp social staging static status storage support terms test
                  uploads webhooks www ]
 
   FORMAT_REGEX = /\A[a-zA-Z0-9]+\z/
