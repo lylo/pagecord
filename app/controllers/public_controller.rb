e91f86db8e5e501a26b8570beef0b68877e62aab
Olly Headey <olly@headey.org>
2026-08-08 08:50:26 +0100

    Remove blog-with-newsletter landing page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,7 +4,7 @@ class PublicController < ApplicationController
   caches_page :terms, :privacy, :faq, :pagecord_vs_hey_world, :pagecord_vs_wordpress,
     :pagecord_vs_substack, :pagecord_vs_about_me, :pagecord_vs_medium,
     :blogging_by_email, :personal_website, :minimalist_blogging,
-    :blog_with_newsletter, :blogger_alternative, :indie_blogging_platform, :brand
+    :blogger_alternative, :indie_blogging_platform, :brand
 
   def terms
   end
@@ -40,9 +40,6 @@ class PublicController < ApplicationController
   end
 
   def blogging_by_email
-  end
-
-  def blog_with_newsletter
   end
 
   def blogger_alternative
