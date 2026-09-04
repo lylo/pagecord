5cc99c11daaad7586375bd087da6f3e1d2f3cce0
Olly Headey <olly@headey.org>
2026-09-04 09:09:23 +0100

    Cap the reply form at 20 a minute
    No human loads a reply form 60 times a minute. 20 matches the other
    anonymous pages: signup, login, home and search. The create cap stays
    at 10 an hour.

--- app/controllers/blogs/posts/replies_controller.rb
+++ app/controllers/blogs/posts/replies_controller.rb
@@ -2,7 +2,7 @@ class Blogs::Posts::RepliesController < Blogs::BaseController
   include SpamPrevention
 
   rate_limit to: 10, within: 1.hour, only: [ :create ], name: "reply-create"
-  rate_limit to: 60, within: 1.minute, only: [ :new ], name: "reply-new"
+  rate_limit to: 20, within: 1.minute, only: [ :new ], name: "reply-new"
 
   skip_forgery_protection # Cached pages have no session cookie for CSRF verification
 
